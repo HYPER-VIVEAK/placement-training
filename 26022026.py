3cceb1c7a98c4f79d8364f61cd97b79e2149eb67
HYPER-VIVEAK <viveakviveak26988@gmail.com>
2025-02-26 11:37:48 +0530

    pattern diamond

--- 26022026.py
+++ 26022026.py
@@ -39,20 +39,41 @@ for i in range(0,n2):
         print(" ",end="")
     print("")
 print("3------------------------------------------------------------------------------------------------------")
-n=int(input("Enter"))
-n=n+1
-for i in range(0,n):#outer loop next line loop
-    for j in range(i,n-1):#prints Space
+# n=int(input("Enter"))
+n=6
+t1=n+1
+for i in range(0,t1):#outer loop next line loop
+    for j in range(i,t1-1):#prints Space
         print(" ",end="")
     for k in range(0,2*i-1):#prints *
         print("*",end="")
     print("")
-n=n-2
-for i in range(0,n):#outer loop next line loop
+t2=t1-2
+for i in range(0,t2):#outer loop next line loop
     for j in range(0,i+1):#prints Space
         print(" ",end="")
-    for k in range(0,2*(n-i)-1):#prints *
+    for k in range(0,2*(t2-i)-1):#prints *
         print("*",end="")
     print("")
 
 print("4--------------------------------------------------------------------------------------------------")
+# n=int(input("Enter"))
+n=6
+t1=n+1
+t2=t1-1
+for i in range(0,t2):#outer loop next line loop
+    for j in range(1,i+1):#prints Space
+        print(" ",end="")
+    for k in range(0,2*(t2-i)-1):#prints *
+        print("*",end="")
+    print("")
+for i in range(0,t1):#outer loop next line loop
+    for j in range(i,t1-1):#prints Space
+        print(" ",end="")
+    for k in range(0,2*i-1):#prints *
+        print("*",end="")
+    print("")
+
+
+
+print("5--------------------------------------------------------------------------------------------------")
