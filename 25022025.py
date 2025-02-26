6e73f2256f49dc1787f63f61e3998cec4e47d2c6
HYPER-VIVEAK <viveakviveak26988@gmail.com>
2025-02-26 09:39:55 +0530

    file day1

--- 25022025.py
+++ 25022025.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+# a = 9#number 10
+# b=bool(not(()))#Modulus 1
+# print(b)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a=10
+# print(a+a)
+# print(a**2)
+# if(""):
+# n=5
+# i=0
+# while(i<n):
+#     print("hi")
+#     i+=1
+#
+# print(i)
+
+# n=9
+# for i in range(0,n,2):
+#    print(i,end=" ")
+
+# n = 5
+# sum=90
+# for i in range(0, n):
+#     print(sum,end=" ")
+#     sum+=5
+# sum=5
+# for i in range(1,5):
+#     print(sum,end=" ")
+#     sum=sum+(5*i)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# s=11
+# for i in range(1,6):
+#     print(s**2,end=" ")
+#     s=s+4
+# for i in range(60,101,10):
+#     print(i*2-1,end=" ")
+#
+# sum=60
+# for i in range(0,5):
+#     print(sum*2-1,end=" ")
+#     sum = sum + 10
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# sum=0
+# d=2
+# for i in range(0,7):
+#     print(sum,end=" ")
+#     sum=sum+d
+#     if(i%2==0):
+#         d=d+4
+# sum=0
+# for i in range(1,8):
+#     if(i%2==0):
+#         sum=((i**2)-2)
+#     else:
+#         sum=((i**2)-1)
+#     print(sum,end=" ")
+#
+#
+# ---------------------------------------------------------------------------------------------------------------------
+    # print(8&-8)
+    # print(7&-7)
+    # print(6&-6)
+    # print(~5)
+    # print(~4)
+# Start your Coding Race
+# a=int(input())
+# if(a==0):
+#   print(False)
+# elif(a==2):
+#     print(True)
+# elif(a&2==0):
+#   print(True)
+# else:
+# #   print(False)
+# a =5
+# print(a<<1)
+
+
+# a=int(input())
+# r="Positive"if(a>0) else ("Negative"if(a<0) else "Zero" )
