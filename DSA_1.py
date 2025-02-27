1e5124a3cc39946394a5e50f9b02fdf716f48a8c
HYPER-VIVEAK <viveakviveak26988@gmail.com>
2025-02-27 10:26:06 +0530

    Lisr

--- DSA_1.py
+++ DSA_1.py
@@ -1,1 +1,36 @@
 #Arrays
+'''
+In array the memory alocation in done by the size of the type of the variable that is for in the memory space is 4 and
+for character it is 1.
+but the memory allocation of the array is static
+
+
+(1)
+vectors
+vectors is a growaable array it multies by 2 that is 4-->--->8-->16--->32--->64
+it can also store same datatype
+
+DSA in pytthon
+List
+
+list in python is not exactly an array it is a vector that behaves like Array
+List is resizable
+List is dynamic sized array which grows and sinks automatically.
+list a Vector which behaves like array but it can store VALUES OF MIXED Data types like int,float, character, string.
+LIst can have  Duplicates
+Lists are ordered, It maintains the order based on how they are ordered.
+List is Mutable
+vectors are powerfull  list is not power full
+list functions are
+sort
+sorted
+add
+remove
+append
+
+
+<-1->
+
+
+'''
+
